18f1ec57b7292c5802dfe938d17bb5431641e3d3
Kristian Gaardsted Bork <70579756+krisbork@users.noreply.github.com>
2023-01-10 11:21:12 +0100

    Updated images of app

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./Home.css";
 import logo from "../../assets/images/logo.png";
-import apppic1 from "../../assets/images/apppic1.png";
-import apppic2 from "../../assets/images/apppic2.png";
-import apppic3 from "../../assets/images/apppic3.png";
+import apppic1 from "../../assets/images/apppic1_v2.webp";
+import apppic2 from "../../assets/images/apppic3_v2.webp";
+import apppic3 from "../../assets/images/apppic4.webp";
 
 function Home() {
     return (
@@ -18,15 +18,15 @@ function Home() {
             
             <div className="app-image-container">
                 <div className="app-image">
-                    <h3>Overblik over køreskolen</h3>
+                    <h3>Overblik over holdstarter</h3>
                     <img src={apppic1} alt="App screenshot 1"/>
                 </div>
                 <div className="app-image">
-                    <h3>Holdstarter med kørelæreren</h3>
+                    <h3>Informationer om eleverne</h3>
                     <img src={apppic2} alt="App screenshot 2"/>
                 </div>
                 <div className="app-image">
-                    <h3>Informationer om eleverne</h3>
+                    <h3>Statistikker over køreskolen</h3>
                     <img src={apppic3} alt="App screenshot 3"/>
                 </div>
             </div>
